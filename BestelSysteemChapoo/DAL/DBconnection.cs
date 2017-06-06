38abc8e6100d5db7520a67e68336e3d75902ddd8
Eigenaar <Eigenaar@LAPTOP-NE82FUTG>
2017-06-06 12:19:17 +0200

    DBconnection.cs gemaakt. In DAO: Sqlconnection conn = DBconnection();

--- BestelSysteemChapoo/DAL/DBconnection.cs
+++ BestelSysteemChapoo/DAL/DBconnection.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
 
 namespace DAL
 {
     class DBconnection
     {
+        // Database connectie wordt geïnitialiseerd
+        SqlConnection conn;
+
+        public DBconnection()
+        {
+            string connString = ConfigurationManager
+                    .ConnectionStrings["ReserveringenConnectionStringSQL"]
+                    .ConnectionString;
+
+            // Database connectie wordt gelegd
+            conn = new SqlConnection(connString);
+
+        }
     }
 }
